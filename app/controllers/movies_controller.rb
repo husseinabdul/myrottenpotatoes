9e1a8567457da8f5646eb2396c8876ac03c78605
Hussein <hussein.abdullahi@mnsu.edu>
2019-04-03 23:40:47 +0000

    fifth commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,36 @@ class MoviesController < ApplicationController
     def index
      @movies = Movie.all
     end
+    def new
+	# default: render ‘new’ template
+    end
+    def create
+      @movie = Movie.create!(params[:movie])
+      redirect_to movies_path
+    end
 end
     
+    
+def show
+  id = params[:id] # retrieve movie ID from URI route
+  @movie = Movie.find(id) # look up movie by unique ID
+  # will render app/views/movies/show.html.haml by default
+end
+
+# in movies_controller.rb
+def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+end
+
+def edit
+  @movie = Movie.find params[:id]
+end
+ 
+def update
+  @movie = Movie.find params[:id]
+  @movie.update_attributes!(params[:movie])
+  flash[:notice] = "#{@movie.title} was successfully updated."
+  redirect_to movie_path(@movie)
+end
